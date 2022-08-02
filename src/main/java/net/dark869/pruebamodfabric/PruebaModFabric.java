09fd4b25e85e874b059d355b29e216245afb4456
Victor Emmanuel <86435465+Dark869@users.noreply.github.com>
2022-08-02 00:19:18 -0500

    Initial commit

---
+++ src/main/java/net/dark869/pruebamodfabric/PruebaModFabric.java
@@ -0,0 +1,15 @@
+package net.dark869.pruebamodfabric;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PruebaModFabric implements ModInitializer {
+	public static final String MOD_ID = "pruebamodfabric";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/dark869/pruebamodfabric/PruebaModFabricClient.java
@@ -0,0 +1,10 @@
+package net.dark869.pruebamodfabric;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PruebaModFabricClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
