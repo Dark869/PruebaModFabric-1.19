e5fa5a271637cd8e46e45fdb655d858718d2b80d
Victor Emmanuel <86435465+Dark869@users.noreply.github.com>
2022-08-03 22:39:41 -0500

    Se introdujo el primer item como prueba

--- src/main/java/net/dark869/pruebamodfabric/PruebaModFabric.java
+++ src/main/java/net/dark869/pruebamodfabric/PruebaModFabric.java
@@ -1,15 +1,20 @@
 package net.dark869.pruebamodfabric;
 
+import net.dark869.pruebamodfabric.item.ModItem;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.rmi.registry.Registry;
 
 public class PruebaModFabric implements ModInitializer {
 	public static final String MOD_ID = "pruebamodfabric";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+
 	@Override
 	public void onInitialize() {
-
+		ModItem.registerModItems();
 	}
+
 }
